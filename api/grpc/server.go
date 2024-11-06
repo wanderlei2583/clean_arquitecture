271d5e6f17b69092bcc1726b05fe4b9db22da3b2
Wanderlei Pereira <devwanderlei@gmail.com>
2024-11-06 19:34:00 -0300

    funçao listar pedidos

--- api/grpc/server.go
+++ api/grpc/server.go
@@ -1,6 +1,10 @@
 package grpc
 
-import "github.com/wanderlei2583/clean_arquitecture/internal/usecase"
+import (
+	"context"
+
+	"github.com/wanderlei2583/clean_arquitecture/internal/usecase"
+)
 
 type ServidorPedidos struct {
 	pb.UnimplementedServicePedidosServer
@@ -11,4 +15,30 @@ func NovoServidorPedidos(uc *usecase.ListarPedidosUseCase) *ServidorPedidos {
 	return &ServidorPedidos{
 		listarPedidoUseCase: uc,
 	}
+}
+
+func (s *ServidorPedidos) ListarPedidos(
+	ctx context.Context,
+	req *pb.RequisicaoListarPedidos,
+) (*pb.RespostaListarPedidos, error) {
+	pedidos, err := s.listarPedidoUseCase.Executar(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	var pbPedidos []*pb.Pedido
+	for _, p := range pedidos {
+		pbPedidos = append(pbPedidos, &pb.Pedido{
+			Id:           p.ID,
+			IdCliente:    p.IDCliente,
+			Status:       p.Status,
+			ValorTotal:   p.ValorTotal,
+			CriadoEm:     p.CriadoEm,
+			AtualizadoEm: p.AtualizadoEm,
+		})
+	}
+
+	return &pb.RespostaListarPedidos{
+		Pedidos: pbPedidos,
+	}, nil
 }
