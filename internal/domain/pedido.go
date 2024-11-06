c811ce00dd5878ae9d3703bea0ada90066e242b0
Wanderlei Pereira <devwanderlei@gmail.com>
2024-11-06 17:36:39 -0300

    criando os pedidos

---
+++ internal/domain/pedido.go
@@ -0,0 +1,12 @@
+package domain
+
+import "time"
+
+type Pedido struct {
+	ID           string    `json:"id"            gorm:"primary_key"`
+	ClienteID    string    `json:"id_cliente"    gorm:"column:id_cliente"`
+	Status       string    `json:"status"`
+	ValorTotal   float64   `json:"valor_total"   gorm:"column:valor_total"`
+	CriadoEm     time.Time `json:"criado_em"     gorm:"column:criado_em"`
+	AtualizadoEm time.Time `json:"atualizado_em" gorm:"column:atualizado_em"`
+}
