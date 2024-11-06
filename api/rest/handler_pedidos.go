d02c3e1f24d4100c22f88462bbd5f9f61d269ad2
Wanderlei Pereira <devwanderlei@gmail.com>
2024-11-06 18:00:02 -0300

    adicionando o gin gonic

--- api/rest/handler_pedidos.go
+++ api/rest/handler_pedidos.go
@@ -1,6 +1,12 @@
 package rest
 
-import "github.com/wanderlei2583/clean_arquitecture/internal/usecase"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+
+	"github.com/wanderlei2583/clean_arquitecture/internal/usecase"
+)
 
 type HandlerPedidos struct {
 	ListarPedidosUseCase *usecase.ListarPedidosUseCase
@@ -10,4 +16,16 @@ func NovoHandlerPedidos(uc *usecase.ListarPedidosUseCase) *HandlerPedidos {
 	return &HandlerPedidos{
 		ListarPedidosUseCase: uc,
 	}
+}
+
+func (h *HandlerPedidos) ListarPedidos(c *gin.Context) {
+	pedidos, err := h.ListarPedidosUseCase.Executar(c.Request.Context())
+	if err != nil {
+		c.JSON(
+			http.StatusInternalServerError,
+			gin.H{"error": "Erro ao buscar pedidos: " + err.Error()},
+		)
+		return
+	}
+	c.JSON(http.StatusOK, pedidos)
 }
