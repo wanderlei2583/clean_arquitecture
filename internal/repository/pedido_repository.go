37554d61240e32f2466b820d7fcfc2a76407c564
Wanderlei Pereira <devwanderlei@gmail.com>
2024-11-06 17:47:36 -0300

    função buscar todos os pedidos

--- internal/repository/pedido_repository.go
+++ internal/repository/pedido_repository.go
@@ -1,6 +1,12 @@
 package repository
 
-import "gorm.io/gorm"
+import (
+	"context"
+
+	"gorm.io/gorm"
+
+	"github.com/wanderlei2583/clean_arquitecture/internal/domain"
+)
 
 type PedidoRepository struct {
 	db *gorm.DB
@@ -10,4 +16,15 @@ func NovoPedidoRepository(db *gorm.DB) *PedidoRepository {
 	return &PedidoRepository{
 		db: db,
 	}
+}
+
+func (r *PedidoRepository) BuscarTodos(
+	ctx context.Context,
+) ([]domain.Pedido, error) {
+	var pedidos []domain.Pedido
+	result := r.db.WithContext(ctx).Find(&pedidos)
+	if result.Error != nil {
+		return nil, result.Error
+	}
+	return pedidos, nil
 }
